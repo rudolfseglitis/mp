2e2e7e40e18d1a8e0464ad6d849d2eab22826b02
rudolfs@eglitis.name <rudolfs@eglitis.name>
2011-07-27 00:13:32 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mp::Application.config.secret_token = 'c5bd661d45e9468e7877f6536d9c7f60eec4bbdfbffb1f6fd7c9b0ee8be47c104095b50fedea1e3656a99f06f070745a3ec5056e8d224ed405c9a59ac98236e1'
