a2b0c051e05c808c55c729f2704e5c00abb8657f
rudolfs@eglitis.name <rudolfs@eglitis.name>
2011-07-27 15:21:06 +0300

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def title
+    base_title  = 'MP'
+    separator   = ' | '
+    if @title.nil?
+      base_title
+    else
+      base_title + separator + @title
+    end
+  end
 end
